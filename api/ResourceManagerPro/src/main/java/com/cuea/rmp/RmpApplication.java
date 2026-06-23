c7fe457e99ff798e8259aaedfcd3f76a76732276
Bindu Kabusara Josue <115138096+bindukabusara@users.noreply.github.com>
2026-06-23 22:22:38 +0300

    Create RmpApplication.java

---
+++ api/ResourceManagerPro/src/main/java/com/cuea/rmp/RmpApplication.java
@@ -0,0 +1,13 @@
+package com.cuea.rmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmpApplication.class, args);
+    }
+
+}
