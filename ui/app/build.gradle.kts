24a6bcfbeeb816bca5583425536acab43319fe84
Munyijwok James Alala <habib.munji@gmail.com>
2026-06-24 20:36:06 +0300

    Update build.gradle.kts

--- ui/app/build.gradle.kts
+++ ui/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
@@ -39,9 +40,6 @@ android {
         buildConfig = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
-    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
