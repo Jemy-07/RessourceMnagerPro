b1e10a2f80f79c8269038fb0448408100723a3b4
Munyijwok James Alala <habib.munji@gmail.com>
2026-06-24 20:35:57 +0300

    Update NetworkModule.kt

--- ui/app/src/main/java/com/cuea/rmp/mobile/core/network/NetworkModule.kt
+++ ui/app/src/main/java/com/cuea/rmp/mobile/core/network/NetworkModule.kt
@@ -13,16 +13,17 @@ import com.cuea.rmp.mobile.resource.ResourceApi
 import com.cuea.rmp.mobile.resource.SkillApi
 import com.cuea.rmp.mobile.timesheet.TimesheetApi
 import com.cuea.rmp.mobile.user.UserApi
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -32,6 +33,7 @@ object NetworkModule {
 
     @Provides
     @Singleton
+    @OptIn(ExperimentalSerializationApi::class)
     fun provideJson(): Json {
         return Json {
             ignoreUnknownKeys = true
