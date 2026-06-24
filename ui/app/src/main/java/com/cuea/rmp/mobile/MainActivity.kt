0e5e69161d60eb4f7f647f69c0a8c080df0482b5
Munyijwok James Alala <habib.munji@gmail.com>
2026-06-24 18:08:58 +0300

    Update MainActivity.kt

--- ui/app/src/main/java/com/cuea/rmp/mobile/MainActivity.kt
+++ ui/app/src/main/java/com/cuea/rmp/mobile/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import com.cuea.rmp.mobile.ui.AppRoot
 import com.cuea.rmp.mobile.ui.auth.AuthViewModel
 import com.cuea.rmp.mobile.ui.notification.NotificationViewModel
+import com.cuea.rmp.mobile.ui.resource.ResourceViewModel
 import com.cuea.rmp.mobile.ui.session.SessionViewModel
 import com.cuea.rmp.mobile.ui.theme.RmpMobileTheme
 import com.cuea.rmp.mobile.ui.timesheet.TimesheetViewModel
@@ -22,6 +23,7 @@ class MainActivity : ComponentActivity() {
     private val authViewModel: AuthViewModel by viewModels()
     private val timesheetViewModel: TimesheetViewModel by viewModels()
     private val notificationViewModel: NotificationViewModel by viewModels()
+    private val resourceViewModel: ResourceViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +37,8 @@ class MainActivity : ComponentActivity() {
                         sessionViewModel = sessionViewModel,
                         authViewModel = authViewModel,
                         timesheetViewModel = timesheetViewModel,
-                        notificationViewModel = notificationViewModel
+                        notificationViewModel = notificationViewModel,
+                        resourceViewModel = resourceViewModel
                     )
                 }
             }
