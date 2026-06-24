f9db67255f4ced8146fd559775ad48cac9215eec
Munyijwok James Alala <habib.munji@gmail.com>
2026-06-24 18:00:16 +0300

    Update AppDatabase.kt

--- ui/app/src/main/java/com/cuea/rmp/mobile/core/db/AppDatabase.kt
+++ ui/app/src/main/java/com/cuea/rmp/mobile/core/db/AppDatabase.kt
@@ -3,15 +3,21 @@ package com.cuea.rmp.mobile.core.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.cuea.rmp.mobile.notification.NotificationDao
+import com.cuea.rmp.mobile.notification.NotificationLocalEntity
+import com.cuea.rmp.mobile.resource.ResourceDao
+import com.cuea.rmp.mobile.resource.ResourceLocalEntity
 import com.cuea.rmp.mobile.timesheet.TimesheetDao
 import com.cuea.rmp.mobile.timesheet.TimesheetLocalEntity
 
 @Database(
     entities = [
         PendingMutationEntity::class,
-        TimesheetLocalEntity::class
+        TimesheetLocalEntity::class,
+        NotificationLocalEntity::class,
+        ResourceLocalEntity::class
     ],
-    version = 1,
+    version = 3,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
@@ -20,6 +26,10 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun pendingMutationDao(): PendingMutationDao
 
     abstract fun timesheetDao(): TimesheetDao
+
+    abstract fun notificationDao(): NotificationDao
+
+    abstract fun resourceDao(): ResourceDao
 }
 
 
