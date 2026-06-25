7e66daa0f69d7f5e0955dbdb7410143f6f254ad8
Munyijwok James Alala <habib.munji@gmail.com>
2026-06-25 20:38:46 +0300

    Update build.gradle.kts

--- ui/app/build.gradle.kts
+++ ui/app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
