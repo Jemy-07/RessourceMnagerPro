1fa2ead7d8e17720e459dc623fb8092789c45456
Munyijwok James Alala <habib.munji@gmail.com>
2026-06-25 20:54:39 +0300

    Update build.gradle.kts

--- ui/app/build.gradle.kts
+++ ui/app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     // --- Background sync ---
     implementation("androidx.work:work-runtime-ktx:2.9.1")
     implementation("androidx.hilt:hilt-work:1.2.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     ksp("androidx.hilt:hilt-compiler:1.2.0")
 
     // --- Token storage ---
@@ -99,6 +100,7 @@ dependencies {
 
     // --- Test ---
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
