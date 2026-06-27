38f960e9d97577740dc4bcdc463f00f26229bf99
Munyijwok James Alala <habib.munji@gmail.com>
2026-06-27 16:23:48 +0300

    Update build.gradle.kts

--- ui/app/build.gradle.kts
+++ ui/app/build.gradle.kts
@@ -107,4 +107,6 @@ dependencies {
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test:core:1.6.1")
+    androidTestImplementation("androidx.work:work-testing:2.9.1")
 }
