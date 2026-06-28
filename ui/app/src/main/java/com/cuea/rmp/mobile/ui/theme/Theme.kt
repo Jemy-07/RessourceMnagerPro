ab0cc67b705fbeebe5e04dc5bbd0f55129ffa60c
Bindu Kabusara Josue <115138096+bindukabusara@users.noreply.github.com>
2026-06-28 18:33:36 +0300

    Update Theme.kt

--- ui/app/src/main/java/com/cuea/rmp/mobile/ui/theme/Theme.kt
+++ ui/app/src/main/java/com/cuea/rmp/mobile/ui/theme/Theme.kt
@@ -5,9 +5,28 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val LightColors = lightColorScheme()
-private val DarkColors = darkColorScheme()
+private val Purple40 = Color(0xFF6650A4)
+private val PurpleGrey40 = Color(0xFF625B71)
+private val Pink40 = Color(0xFF7D5260)
+private val Purple80 = Color(0xFFD0BCFF)
+private val PurpleGrey80 = Color(0xFFCCC2DC)
+private val Pink80 = Color(0xFFEFB8C8)
+
+private val LightColors = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40,
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+)
+
+private val DarkColors = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80,
+)
 
 @Composable
 fun RmpMobileTheme(
@@ -19,4 +38,3 @@ fun RmpMobileTheme(
         content = content
     )
 }
-
